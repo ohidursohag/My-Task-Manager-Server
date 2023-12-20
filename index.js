e304f935099cc4b48ca0bb5cc5a59305569bee3b
ohidursohag <ohidursohag9595@gmail.com>
2023-12-21 00:21:29 +0600

    setup express server and connet with mongodb

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000 || 5001 || 5002
+
+const app = express();
+
+// middleware
+app.use(cors({
+   origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      ""
+   ],
+   credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser())
+
+
+const uri = process.env.MONGO_DB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      // client.connect();
+      // Send a ping to confirm a successful connection
+      client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+   }
+}
+run().catch(console.dir);
+
+// Database Collection
+const userCollection = client.db('eParcelDB').collection('users');
+
+// Test Api
+app.get('/', (req, res) => {
+   res.send('Server is Running');
+})
+app.listen(port, () => {
+   console.log(`server listening on port ${port}`);
+});
